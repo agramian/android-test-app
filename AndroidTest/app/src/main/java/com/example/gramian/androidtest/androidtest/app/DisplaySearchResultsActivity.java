901e11adaab2d9aeecf7f6261a9e6685c89727d1
agramian <abtin.gramian@gmail.com>
2015-10-08 00:11:42 -0700

    refactored to add app and rest packages
    - moved activity code to app package
    - created new rest package

--- AndroidTest/app/src/main/java/com/example/gramian/androidtest/androidtest/app/DisplaySearchResultsActivity.java
+++ AndroidTest/app/src/main/java/com/example/gramian/androidtest/androidtest/app/DisplaySearchResultsActivity.java
@@ -1,17 +1,14 @@
-package com.example.gramian.androidtest;
+package com.example.gramian.androidtest.androidtest.app;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+
+import com.example.gramian.androidtest.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,13 +30,10 @@ public class DisplaySearchResultsActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.search_results);
         List<String> resultList = new ArrayList<String>();
         resultList.add(searchQuery);
-        // This is the array adapter, it takes the context of the activity as a
-        // first parameter, the type of list view as a second parameter and your
-        // array as a third parameter.
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
-                resultList );
+                resultList);
 
         listView.setAdapter(arrayAdapter);
         new AlertDialog.Builder(this)
--- AndroidTest/app/src/main/java/com/example/gramian/androidtest/androidtest/app/MainActivity.java
+++ AndroidTest/app/src/main/java/com/example/gramian/androidtest/androidtest/app/MainActivity.java
@@ -1,15 +1,15 @@
-package com.example.gramian.androidtest;
+package com.example.gramian.androidtest.androidtest.app;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+
+import com.example.gramian.androidtest.R;
+import com.example.gramian.androidtest.androidtest.app.DisplaySearchResultsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
