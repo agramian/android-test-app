203daa7256b178529b9e7616df0be26bf1e8ff84
agramian <abtin.gramian@gmail.com>
2015-10-07 23:49:26 -0700

    displaying array of strings in list view
    - modified search results views to have list view element
    - displaying array of strings as rows in a list view

--- AndroidTest/app/src/main/java/com/example/gramian/androidtest/DisplaySearchResultsActivity.java
+++ AndroidTest/app/src/main/java/com/example/gramian/androidtest/DisplaySearchResultsActivity.java
@@ -7,14 +7,22 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DisplaySearchResultsActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_display_search_results);
+        /*
         //setContentView(R.layout.activity_display_search_results);
 
         // Get the query from the intent
@@ -29,6 +37,23 @@ public class DisplaySearchResultsActivity extends AppCompatActivity {
 
         // Set the text view as the activity layout
         setContentView(textView);
+        */
+        listView = (ListView) findViewById(R.id.search_results);
+        // Instanciating an array list (you don't need to do this,
+        // you already have yours).
+        List<String> resultList = new ArrayList<String>();
+        resultList.add("foo");
+        resultList.add("bar");
+
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                resultList );
+
+        listView.setAdapter(arrayAdapter);
     }
 
 }
