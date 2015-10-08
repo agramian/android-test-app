8e88951854f2b8224bf35bb753616d83e5c69018
Leland Takamine <leland.takamine@gmail.com>
2015-10-08 14:38:48 -0700

    Remove unused code

--- AndroidTest/app/src/main/java/com/example/gramian/androidtest/androidtest/app/MainActivity.java
+++ AndroidTest/app/src/main/java/com/example/gramian/androidtest/androidtest/app/MainActivity.java
@@ -5,12 +5,9 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 
 import com.example.gramian.androidtest.R;
-import com.example.gramian.androidtest.androidtest.app.DisplaySearchResultsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,23 +21,6 @@ public class MainActivity extends AppCompatActivity {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void search(View view) {
